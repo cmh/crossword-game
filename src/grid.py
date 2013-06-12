75634afd8dc5c5fd901ce2be5f62061a8c7a986e
Chris Holdsworth <chrisholdsworth@gmail.com>
2013-06-12 22:10:27 +0100

    Allow grid_size to be variable and make main_game more generic, allowing multiple runs (for profiling, performance etc.)

--- src/grid.py
+++ src/grid.py
@@ -1,4 +1,4 @@
-from config import grid_size, null_char
+from config import grid_size, null_char, min_word_length
 import config
 
 # Thrown if a player input is incorrect
@@ -7,12 +7,13 @@ class InputError(Exception):
         self.msg = desc
     def __str__(self):
         return self.msg
-        
+
 class BaseGrid(object):
-    def __init__(self):
-        self.grid = [0] * grid_size * grid_size
-    def __init__(self, grid):
-        self.grid = list(grid.grid)
+    def __init__(self, grid=None):
+        if grid:
+            self.grid = list(grid.grid)
+        else:
+            self.grid = [0] * grid_size * grid_size
 
     # returns the letter at the specified grid location
     def getLetter(self, row, col):
@@ -21,7 +22,7 @@ class BaseGrid(object):
     # prints the grid
     def __str__(self):
         return '\n'.join([''.join(self.getLetterLine(i)) for i in range(grid_size, grid_size*2)])
-        
+
     # returns the 5 letters at the specified starting position
     # 0-4 are the horizontal words from the top
     # 5-9 are the vertical words from the left
@@ -31,34 +32,30 @@ class BaseGrid(object):
         return [self.getLetter(i - grid_size, j) for j in range(0, grid_size)]
 
 # returns the 5 possible word configurations
-# TODO make this generic for grid_size
 def getPossibleWordsFromLetterLine(letters):
     words = []
-    words.append(letters[0:5])
-    words.append(letters[0:4])
-    words.append(letters[0:3])
-    words.append(letters[1:4])
-    words.append(letters[1:5])
-    words.append(letters[2:5])
+    for i in xrange(0, grid_size+1-min_word_length):
+        for j in xrange(i+min_word_length, grid_size+1):
+            words.append(letters[i:j])
     return [''.join(w) for w in words]
 
 # returns a list of words from within the set of letters that are in the dictionary
 def getAllValidWordsForLetterLine(letters):
     words = getPossibleWordsFromLetterLine(letters)
     return [w for w in words if w in config.dictionary]
-    
-class Grid(BaseGrid): 
+
+class Grid(BaseGrid):
     def __init__(self):
         self.grid = [null_char] * grid_size * grid_size
 
     # sets the letter at the specified location
     def setLetter(self, row, col, letter, force=False):
-        if row < 0 or row > 4:
+        if row < 0 or row >= grid_size:
             raise InputError("Row out of bounds: %x" % row)
-        if col < 0 or col > 4:
+        if col < 0 or col >= grid_size:
             raise InputError("Col out of bounds: %x" % col)
         if self.getLetter(row, col) != null_char and not force:
-            raise InputError("Square already populated (%x,%x) = %s" % (row, col, self.getLetter(row, col))) 
+            raise InputError("Square already populated (%x,%x) = %s" % (row, col, self.getLetter(row, col)))
         self.grid[col * grid_size + row] = letter
 
     # returns a list of scoring words from the Grid
--- src/main_game.py
+++ src/main_game.py
@@ -1,5 +1,6 @@
 from grid import Grid, BaseGrid
 from config import grid_size, null_char
+from random import shuffle
 
 # Represents a single player in the game
 # Each player has a personality which decides the strategy
@@ -7,7 +8,22 @@ class PlayerHolder(object):
     def __init__(self, personality):
         self.personality = personality
         self.grid = Grid()
-        
+
+
+class LeaderboardEventHandler(object):
+    def letterPlaced(self, player, letter, coord):
+        pass
+
+    def gameComplete(self, leaderboard):
+        print "Scoreboard:"
+        print '\n'.join(["%s - %d [%d]" % (l.personality.name, l.score, l.cum_score) for l in leaderboard])
+
+    def letterChosen(self, player, letter):
+        pass
+    def newTurn(self, turnNumber):
+        print "# Turn", turnNumber
+
+
 class ConsoleGameEventHandler(object):
     def letterPlaced(self, player, letter, coord):
         print player.personality.name, 'placed', letter, 'at', coord
@@ -15,34 +31,37 @@ class ConsoleGameEventHandler(object):
     def gameComplete(self, leaderboard):
         print "Scoreboard:"
         print '\n'.join(["%s - %d" % (l.personality.name, l.score) for l in leaderboard])
-        
+
         for player in leaderboard:
             print '===', player.personality.name, '==='
             print 'Grid:'
             print player.grid
             print 'Words:'
             print ', '.join(["%s %x" % (w, len(w)) for w in player.scoring_words])
-            print 'Total:', player.score            
+            print 'Total:', player.score
     def letterChosen(self, player, letter):
         pass
     def newTurn(self, turnNumber):
         print "# Turn", turnNumber
-    
-def playGame(player_personalities, game_event_handler=ConsoleGameEventHandler()):
+
+def playGame(player_personalities, game_event_handler=ConsoleGameEventHandler(), num_games=1):
     players = [PlayerHolder(p) for p in player_personalities]
-    
+
+    for player in players:
+        player.cum_score = 0
+
     def chooseLetter(player):
-        letter = player.personality.chooseLetter(BaseGrid(player.grid)).lower()
+        letter = player.personality.chooseLetter(player.grid).lower()
         game_event_handler.letterChosen(player, letter)
         return letter
-    
+
     def placeLetter(player, letter):
-        coord = player.personality.placeLetter(BaseGrid(player.grid), letter)
+        coord = player.personality.placeLetter(player.grid, letter)
         if (player.grid.getLetter(coord[0], coord[1]) != null_char):
             raise InputException("Cannot place letter at %x,%x" % coord)
         game_event_handler.letterPlaced(player, letter, coord)
         return player.grid.setLetter(coord[0], coord[1], letter)
-        
+
     def completeGame():
         for player in players:
             # take last turn
@@ -53,24 +72,32 @@ def playGame(player_personalities, game_event_handler=ConsoleGameEventHandler())
             assert null_char not in player.grid.grid
             player.scoring_words = player.grid.getScoringWords()
             player.score = sum([len(x) for x in player.scoring_words])
+            player.cum_score += player.score
 
-        leaderboard = sorted(players, key=lambda player: player.score, reverse=True)
+        leaderboard = sorted(players, key=lambda player: player.cum_score, reverse=True)
         return leaderboard
 
-    for player in players:
-        player.grid = Grid()
-        
-    turn = 1
-    while True: 
-        for player in players:
-            game_event_handler.newTurn(turn)
-            if turn > grid_size * grid_size - 1:
-                leaderboard = completeGame()                
-                game_event_handler.gameComplete(leaderboard)
-                return leaderboard
-                
-            turn += 1
-            letter = chooseLetter(player)
 
-            for player in players:  
-                placeLetter(player, letter)
+    def play_game():
+        turn = 1
+        for player in players:
+            player.grid = Grid()
+        shuffle(players)
+
+        while True:
+            for player in players:
+                game_event_handler.newTurn(turn)
+                if turn > grid_size * grid_size - 1:
+                    leaderboard = completeGame()
+                    game_event_handler.gameComplete(leaderboard)
+                    return
+
+                turn += 1
+                letter = chooseLetter(player)
+
+                for player in players:
+                    placeLetter(player, letter)
+
+    for i in xrange(num_games):
+        print "Starting game {}".format(i)
+        play_game()
